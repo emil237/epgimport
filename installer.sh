65ecec6aaf93b9bbc8ff38842f8eab30ceeb6217
emil237 <95539537+emil237@users.noreply.github.com>
2022-02-09 19:24:28 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
-##setup command=wget https://raw.githubusercontent.com/emil237/epgimport/main/installer.sh -O - | /bin/sh
-#
-echo " download and install plugin xstreamity "
-################################################################################
-version=1.3
-OPKGINSTALL=opkg install --force-overwrite
+#wget -q "--no-check-certificate" https://raw.githubusercontent.com/emil237/epgimport/main/installer.sh -O - | /bin/sh
+
+##########################################
+version=4.4.0
+#############################################################
+TEMPATH=/tmp
+OPKGINSTALL="opkg install --force-reinstall"
+MY_IPK="EPGImport-mod-dorik1972_1.8.5.1_all.ipk"
+MY_DEB="EPGImport-mod-dorik1972_1.8.5.1_all.deb"
 MY_URL="https://raw.githubusercontent.com/emil237/epgimport/main"
-MY_IPK="EPGImport-mod_1.3_all.ipk"
-MY_DEB="EPGImport-mod_1.3_all.deb"
-##############################################################################
-# Download and install plugin #
-echo " DOWNLOAD AND INSTALL PLUGIN "
-cd /tmp 
-rm -rf * > /dev/null 2>&1
+# remove old version #
+rm -rf /usr/lib/enigma2/python/Plugins/Extensions/EPGImport
+
+echo ""
+# Download and install plugin
+cd /tmp
 set -e
      wget "$MY_URL/$MY_IPK"
   wait
@@ -22,14 +24,14 @@ set -e
  if which dpkg > /dev/null 2>&1; then
 		dpkg -i --force-overwrite $MY_DEB; apt-get install -f -y
 	else
-		opkg install --force-reinstall $MY_IPK
+		$OPKGINSTALL $MY_IPK
 	fi
-echo "==========================================================================================================================="
+echo "================================="
 set +e
 cd ..
 wait
-rm -f /tmp/$MY_IPK
-rm -f /tmp/$MY_DEB
+rm -f /tmp/*.ipk
+rm -f /tmp/*.deb
 	if [ $? -eq 0 ]; then
 echo ">>>>  SUCCESSFULLY INSTALLED <<<<"
 fi
@@ -41,68 +43,3 @@ echo "**************************************************************************
 wait
 killall -9 enigma2
 exit 0
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
